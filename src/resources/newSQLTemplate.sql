aa5725a82f00b7627cf57efa59e82e8fdb8bc6c3
Sivakumar Gollu <sivakumargllu@gmail.com>
2015-09-04 08:55:13 -0800

    updating modified table structure of emp_attendence

--- src/resources/newSQLTemplate.sql
+++ src/resources/newSQLTemplate.sql
@@ -1,5 +1,8 @@
- CREATE TABLE `emp_attendence` (
- `date` datetime DEFAULT NULL,
- `in_time` datetime DEFAULT NULL,
- `out_time` datetime DEFAULT NULL
- )ENGINE=InnoDB DEFAULT CHARSET=latin1 |
+| emp_attendence | CREATE TABLE `emp_attendence` (
+  `date` datetime DEFAULT NULL,
+  `in_time` datetime DEFAULT NULL,
+  `out_time` datetime DEFAULT NULL,
+  `emp_id` int(4) DEFAULT NULL,
+  `record_id` int(4) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`record_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 |
